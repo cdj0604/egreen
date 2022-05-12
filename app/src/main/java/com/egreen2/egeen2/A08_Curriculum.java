d8c411831925fa25dfb942a3f8f48a3ac64120ec
JDirectory <git.egreen.mobile@gmail.com>
2022-05-12 09:39:21 +0900

    2022.05.12

--- app/src/main/java/com/egreen2/egeen2/A08_Curriculum.java
+++ app/src/main/java/com/egreen2/egeen2/A08_Curriculum.java
@@ -3,39 +3,32 @@ package com.egreen2.egeen2;
 
 import android.app.ProgressDialog;
 import android.content.ContentValues;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.egreen2.egeen2.Adapter.A08_CurriculumAdapter;
-import com.egreen2.egeen2.Data.A08_CurriculumListData;
 import com.egreen2.egeen2.Data.StudyInfo;
 import com.google.android.material.navigation.NavigationView;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
-import java.util.ArrayList;
+import java.io.IOException;
 
 import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -48,11 +41,13 @@ import androidx.drawerlayout.widget.DrawerLayout;
     [작성일시]
  */
 
-public class A08_Curriculum extends AppCompatActivity implements NetworkAsyncTasker.AsyncResponse {
+public class A08_Curriculum extends AppCompatActivity {
     private static final String TAG = A08_Curriculum.class.getSimpleName();
     String id, userName, loginNumber;
     StudyInfo si;
     String iniUrl = "";
+    String list, list1 , list2;
+    TextView textView, textView2, textView3;
     ProgressDialog loadingDialog;
     ListView a08_curriculumList;    //교육과정 리스트
     //AsyncTask 결과 값을 받기 위한 변수
@@ -67,6 +62,55 @@ public class A08_Curriculum extends AppCompatActivity implements NetworkAsyncTas
         super.onCreate(savedInstanceState);
         setContentView(R.layout.a08_curriculum);
         si = (StudyInfo) getIntent().getSerializableExtra("studyInfo");
+        textView = (TextView) findViewById(R.id.textView);
+        textView2 = (TextView) findViewById(R.id.textView2);
+        textView3 = (TextView) findViewById(R.id.textView3);
+        final Bundle bundle = new Bundle();
+
+        new Thread() {
+            @Override
+            public void run() {
+                Document doc = null;
+                try {
+                    //과목명
+                    doc = Jsoup.connect("https://cb.egreen.co.kr/Contents/test/list.html").get();
+                    Elements contents = doc.select("#tab01 > table > tbody > tr:nth-child(1) > td.t_l_i.f_b > a");          //회차 id값 가져오기
+                    list1 = contents.text() + "\n";
+
+                    for (int i = 2; i < 69; i++) {
+                        contents = doc.select("#tab01 > table > tbody > tr:nth-child(" + i + ") > td.t_l_i.f_b > a");
+                        list1 += contents.text() + "\n";
+                    }
+
+                    //이수구분
+                    Elements contents1 = doc.select("#tab01 > table > tbody > tr:nth-child(1) > td:nth-child(1)");
+                    list = contents1.text() + "\n";
+                    for (int i = 2; i < 69; i++) {
+                        contents = doc.select("#tab01 > table > tbody > tr:nth-child( " + i + " ) > td:nth-child(1)");
+                        list += contents.text() + "\n";
+                    }
+
+                    //교수명
+                    Elements contents2 = doc.select("#tab01 > table > tbody > tr:nth-child(1) > td:nth-child(6)");
+                    list2 = contents2.text() + "\n";
+                    for (int i = 2; i < 69; i++) {
+                        contents = doc.select("#tab01 > table > tbody > tr:nth-child( " + i + " ) > td:nth-child(6)");
+                        list2 += contents.text() + "\n";
+                    }
+
+                    bundle.putString("list1", list1);                               //핸들러를 이용해서 Thread()에서 가져온 데이터를 메인 쓰레드에 보내준다.
+                    bundle.putString("list2", list2);                               //핸들러를 이용해서 Thread()에서 가져온 데이터를 메인 쓰레드에 보내준다.
+                    bundle.putString("list", list);                               //핸들러를 이용해서 Thread()에서 가져온 데이터를 메인 쓰레드에 보내준다.
+                    Message msg = handler.obtainMessage();
+                    msg.setData(bundle);
+                    handler.sendMessage(msg);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
         /* Toolbar */
         Toolbar toolbar = (Toolbar) findViewById(R.id.a08_toolbar);
         setSupportActionBar(toolbar);
@@ -87,26 +131,6 @@ public class A08_Curriculum extends AppCompatActivity implements NetworkAsyncTas
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         ImageView imageView = findViewById(R.id.logo);
 
-        /* 웹뷰가 모두 로딩될때까지 보여줄 Dialog */
-        loadingDialog = new ProgressDialog(this);
-        loadingDialog.setCancelable(false);
-        loadingDialog.setMessage("잠시만 기다려주세요.");
-        loadingDialog.setIndeterminate(true);
-        loadingDialog.setCancelable(false);
-        loadingDialog.setCanceledOnTouchOutside(false);
-
-        /* WebView 설정 */
-        webView = findViewById(R.id.webview);//코드연결
-        webView.getSettings().setJavaScriptEnabled(true);//자바스크립트 허용
-        webView.loadUrl("http://mcb.egreen.co.kr/m/default2.asp");
-        webView.setWebChromeClient(new WebChromeClient());//웹뷰에 크롬 사용 허용//이 부분이 없으면 크롬에서 alert가 뜨지 않음
-        webView.setWebViewClient(new WebBrowserClient());//새창열기 없이 웹뷰 내에서 다시 열기//페이지 이동 원활히 하기위해 사
-        webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-        webView.clearCache(true);
-        webView.getSettings().setTextZoom(100);  //웹뷰 폰트 크기 고정
-        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
-        webView.getSettings().setSupportMultipleWindows(true);
-        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
 
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -115,13 +139,6 @@ public class A08_Curriculum extends AppCompatActivity implements NetworkAsyncTas
                 startActivity(intent);
             }
         });
-        /* 네트워크 연결 상태 확인 */
-        netCheck = new NetworkStateCheck(this);
-        if (netCheck.isConnectionNet()) {
-            init();
-        } else {
-            Toast.makeText(A08_Curriculum.this, "네트워크 연결 실패", Toast.LENGTH_SHORT).show();
-        }
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         //네비게이션 헤더에 참조할때는 아래와같이 참조해야한다.
@@ -174,23 +191,18 @@ public class A08_Curriculum extends AppCompatActivity implements NetworkAsyncTas
                 return true;
             }
         });
-        init();
+
     } //onCreate 종료
 
-    private void init() {
-        /* 교육과정 */
-        netConnForGetCurriculum();
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {//뒤로가기 버튼 이벤트
-        if (!(!(keyCode == KeyEvent.KEYCODE_BACK) || !webView.canGoBack())) {
-            webView.goBack();
-            return true;
-        } else {
-            return super.onKeyDown(keyCode, event);
-        }//웹뷰에서 뒤로가기 버튼을 누르면 뒤로가짐
-    }
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Bundle bundle = msg.getData();
+            textView.setText(bundle.getString("list1"));                      //이런식으로 View를 메인 쓰레드에서 뿌려줘야한다.
+            textView2.setText(bundle.getString("list"));                      //이런식으로 View를 메인 쓰레드에서 뿌려줘야한다.
+            textView3.setText(bundle.getString("list2"));                      //이런식으로 View를 메인 쓰레드에서 뿌려줘야한다.
+        }
+    };
 
     @Override
     protected void onStart() {
@@ -202,93 +214,6 @@ public class A08_Curriculum extends AppCompatActivity implements NetworkAsyncTas
         super.onResume();
     }
 
-    /**
-     * 교육과정을 받아오는 웹 서버의 통신
-     */
-    private void netConnForGetCurriculum() {
-        Log.i(TAG, "통신");
-
-        String url = "http://cb.egreen.co.kr/mobile_proc/index_m2.asp";
-
-        asyncTask = new NetworkAsyncTasker(this, url, null);
-        asyncTask.execute();
-    }
-
-    /**
-     * 교육과정 AsyncTask 결과
-     */
-    @Override
-    public void processFinish(String result, String what) {
-        Log.i(TAG, "=======>>" + result);
-
-        if (result.equals("FAIL")) {
-            //통신오류
-            Log.i(TAG, "통신오류");
-        } else {
-            parsing_json(result);
-            Log.i(TAG, "값을 가져옴!!");
-        }
-    }
-
-    private void parsing_json(String result) {
-        JSONObject jsObj = new JSONObject();
-        final ArrayList<A08_CurriculumListData> curriculumListData = new ArrayList<A08_CurriculumListData>();
-
-        try {
-            JSONArray jsArray = new JSONArray(result);
-
-            for (int i = 0; i < jsArray.length(); i++) {
-                jsObj = jsArray.getJSONObject(i);
-
-                String cClassName = jsObj.getString("cClassName");
-                String cClassId = jsObj.getString("cClassId");
-                String DirectoryName = jsObj.getString("DirectoryName");
-
-                /* ListView에 data를 넣기 위한 Adapter 설정 */
-                A08_CurriculumAdapter a08_curriculumAdapter = new A08_CurriculumAdapter(getApplicationContext(), R.layout.a08_curriculum_c, curriculumListData);
-                a08_curriculumAdapter.notifyDataSetChanged();
-                a08_curriculumList.setAdapter(a08_curriculumAdapter);
-                a08_curriculumList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        if (netCheck.isConnectionNet()) {
-                            Intent intent = new Intent(A08_Curriculum.this, A08_LearningPlan.class);
-                            //intent.putExtra("NOTICE", curriculumListData.get(position).getDirectoryName());
-                            startActivity(intent);
-                        } else {
-                            Toast.makeText(A08_Curriculum.this, "네트워크 연결 실패", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-            }
-        } catch (JSONException e) {
-            Log.i(TAG, "curriculum JSON Exc: " + e.getMessage());
-        }
-    }
-
-    public void go_logout(View view) {
-        //다이얼로그 예 눌렀을때 인텐트실행
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-        builder.setMessage("로그아웃 하시겠습니까?");
-
-        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
-                Intent intent = new Intent(getApplicationContext(), before_Main.class);
-                startActivity(intent);
-
-            }
-        });
-
-        builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
-            }
-        });
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
-    }
 
     private void moveActivity(Class activity) {
         StudyInfo si = new StudyInfo();
@@ -395,6 +320,7 @@ public class A08_Curriculum extends AppCompatActivity implements NetworkAsyncTas
                 }
             }
         }
+
 
     }
 }
